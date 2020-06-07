db9a065fc7c601cc631cdf2d0f5872aea516651d
Prathamesh Purandare <pratham.purandare@gmail.com>
2020-06-07 22:35:21 +0530

    Initial Commit

--- Function1.cs
+++ Function1.cs
@@ -13,9 +13,7 @@ namespace HTTPTriggerFunction
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
